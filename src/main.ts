f88b40d2209e6008db744672fbda7ca2ea2a2613
Andrew <andreyka.zaytsev.2017@mail.ru>
2025-07-25 14:52:43 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://table-client.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
-
 bootstrap();
